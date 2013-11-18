d3f72a4241a88ada774ba7b713a56aca173f589c
xiaoghu <hxgqh@126.com>
2013-11-18 14:19:38 +0800

    coding

--- positions/models.py
+++ positions/models.py
@@ -16,7 +16,7 @@ class PositionCardModel(models.Model):
 class PositionsModel(models.Model):
     ip = models.GenericIPAddressField(verbose_name="IP地址")
     mac = models.CharField(verbose_name="Mac地址", max_length=255, blank=True, null=True)
-    position = models.CharField(verbose_name="地址名称", max_length=255, unique=True)
+    position = models.CharField(verbose_name="地址名称", max_length=255)
     install_position = models.CharField(verbose_name="安装位置", max_length=255)
     position_card = models.ForeignKey(PositionCardModel, blank=True, null=True)  # position_card_id
 

f6f14b90a9fe0c220aa4965d6a053e3161701b8c
xiaoghu <hxgqh@126.com>
2013-11-19 13:36:39 +0800

    coding

--- test/test_daemon.py
+++ test/test_daemon.py
@@ -3,7 +3,7 @@
 
 __author__ = 'xiaoghu@cisco.com'
 
-
+import socket
 from backend.get_data import *
 from backend.utils import *
 from jailMonitor import *
@@ -25,7 +25,19 @@ class tDaemon(object):
 
     @staticmethod
     def test_send_temp_hum_query():
-
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind(('localhost', 8008))
+        sock.listen(5)
+        while True:
+            connection, address = sock.accept()
+            try:
+                connection.settimeout(5)
+                buf = connection.recv(1024)
+                print "buf:"+hexlify(buf)
+                connection.send('01040000000271CB')
+            except socket.timeout:
+                print 'time out'
+            connection.close()
         pass
 
 
